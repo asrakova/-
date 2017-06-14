423513a2b1f858b3421ce5a98a3c3df38284ebd5
asrakova <asrakova@edu.hse.ru>
2017-06-14 09:09:16 +0500

    Create Program.cs

--- Program.cs
+++ Program.cs
@@ -26,6 +26,42 @@ namespace ConsoleApplication1
 
     class Program
     {
+        public Sys[] history = new Sys[4];
+        public Sys now;
+        
+        public void NewSystem(double x, double y, double z, double c, int i)
+        {
+            now.matrix[i, 0] = x;
+            now.matrix[i, 0] = y;
+            now.matrix[i, 0] = z;
+            now.matrix[i, 0] = c;
+        }
+        
+        public void Count()
+        {
+                        for (int k = 0; k < 2; k++)
+                for (int i = k + 1; i < 3; i++)
+                    if (a.matrix[i, k] != 0)
+                    {
+                        kof = -a.matrix[k, k] / a.matrix[i, k];
+                        for (int j = 0; j < 4; j++)
+                            a.matrix[i, j] = a.matrix[k, j] + a.matrix[i, j] * kof;
+                    }
+                    else Console.WriteLine("Система не решаема");
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                    Console.Write("{0:0.000} ", a.matrix[i,j]);
+                Console.WriteLine();
+            }
+
+            a.xxx[2] = a.matrix[2, 3] / a.matrix[2, 2];
+            a.xxx[1] = (a.matrix[1, 3] - a.xxx[2] * a.matrix[1, 2]) / a.matrix[1, 1];
+            a.xxx[0] = (a.matrix[0, 3] - a.xxx[2] * a.matrix[0, 2] - a.xxx[1]* a.matrix[0,1]) / a.matrix[0, 0];
+        }
+        
+        
         static void Main(string[] args)
         {
             double kof;
